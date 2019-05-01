81da7c86cadde0caef9fb7ac9915efa16560f4b1
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-05-01 19:11:32 +0900

    最低限機能するもの

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,9 +1,11 @@
+# ライブラリのインポート
+import os
 
 # 環境変数に定義しておく
-API_TOKEN = "xoxb-46717260934-622020920560-09mtqoeYxCyBANvaCdKHeSsR"
+API_TOKEN = 'xoxb-46717260934-622020920560-manGX2nOhL2yKRmcxaA4EYkk'
 
 # デフォルトの応答
-DEFAULT_REPLY = "認識外のコマンドです"
+DEFAULT_REPLY = "すみません。よくわかりません"
 
 PLUGINS = [
             'slackbot.plugins',
