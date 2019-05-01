a9e27d3371d6e4d25549c9e08794b28bf356c749
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-05-01 14:53:52 +0900

    slackbot

---
+++ slackbot/slackbot_settings.py
@@ -0,0 +1,12 @@
+
+# 環境変数に定義しておく
+API_TOKEN = "xoxb-46717260934-622020920560-09mtqoeYxCyBANvaCdKHeSsR"
+
+# デフォルトの応答
+DEFAULT_REPLY = "認識外のコマンドです"
+
+PLUGINS = [
+            'slackbot.plugins',
+            'botmodules.conversation',
+            # ここにカンマ区切りでプラグインを追加していくことで拡張できます。
+]
