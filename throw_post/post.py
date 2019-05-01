43fa85def539eb7aaf3d9b70213fd1894755a161
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-05-01 13:34:00 +0900

    first commit

---
+++ throw_post/post.py
@@ -0,0 +1,16 @@
+import pprint
+import requests
+
+def main():
+    #POSTパラメータは二つ目の引数に辞書で指定する
+    response = requests.post(
+        'http://127.0.0.1:5000/post',
+        {'foo':'bar'})
+    #レスポンスオブジェクトのjsonメソッドを使うと、
+    #JSONデータをPythonの辞書オブジェクトに変換して取得できる
+    print(response)
+    print(response.url)
+    print(response.headers)
+    print(response.text)
+if __name__=='__main__':
+    main()
