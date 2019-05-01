816c8ad3b1ca26d34a4807a63272e67a4f658d02
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-05-01 14:54:21 +0900

    APIの確認

--- throw_post/post.py
+++ throw_post/post.py
@@ -3,15 +3,16 @@ import requests
 
 def main():
     #POSTパラメータは二つ目の引数に辞書で指定する
-    response = requests.post(
-        'http://127.0.0.1:5000/post',
-        {'foo':'bar'})
+    url = 'http://127.0.0.1:5000/post'
+    payload = {'foo':'bar'}
+    response = requests.post(url, payload)
     #レスポンスオブジェクトのjsonメソッドを使うと、
     #JSONデータをPythonの辞書オブジェクトに変換して取得できる
     print(response)
     print(response.url)
     print(response.headers)
     print(response.text)
+    
 if __name__=='__main__':
     main()
 
