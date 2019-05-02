fc06e2be71ffd803d2199ea2c61b0e920d7f0dec
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-05-02 16:17:30 +0900

    保存画像名を日時に変更

--- save_server/server.py
+++ save_server/server.py
@@ -1,26 +1,31 @@
+import os
+import base64
+import codecs
+from datetime import datetime
 from flask import Flask, request
-import requests, urllib3, os, shutil, base64, codecs
 import server_setting
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello! It's sotaServer in SIT !"
+    return "Hello! This is sotaServer in SIT !"
 
 @app.route("/post", methods=['GET', 'POST'])
 def post():
     if request.method == 'POST':
         enc_img = request.form['enc_img']
+        ex = request.form['extension']
         content = base64.b64decode(enc_img)
-
-        with codecs.open(server_setting.SAVE_PATH + request.form['name'], 'wb') as fo:
+        time = datetime.now().strftime("%Y%m%d-%H%M%S")
+        if not os.path.isfile(server_setting.SAVE_PATH):
+            os.makedirs(server_setting.SAVE_PATH)
+        with codecs.open(server_setting.SAVE_PATH + time + ex, 'wb') as fo:
             fo.write(content)
-        
-        return 'aaa'
+        return 'OK'
     else:
-        # hoge = request.form['foo']
-        return 'GET'   
- 
+        return 'GET'
+
 
 if __name__ == '__main__':
     # app.run(debug=False, host='0.0.0.0', port=80)
