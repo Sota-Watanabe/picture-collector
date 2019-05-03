9859b3874e21fedacb65bc25e5ba08116be73162
Sota-Watanabe <cy16179@shibaura-it.ac.jp>
2019-05-03 16:49:10 +0900

    makedirあたりの例外処理追加、 ip、ポートの説明追加

--- receive_post/server.py
+++ receive_post/server.py
@@ -18,14 +18,19 @@ def post():
         ex = request.form['extension']
         content = base64.b64decode(enc_img)
         time = datetime.now().strftime("%Y%m%d-%H%M%S")
-        os.makedirs(server_setting.SAVE_PATH, exist_ok=True)
-        with codecs.open(server_setting.SAVE_PATH + '/' + time + ex, 'wb') as fo:
-            fo.write(content)
-        return 'OK'
+        try:
+            os.makedirs(server_setting.SAVE_PATH, exist_ok=True)
+            with codecs.open(server_setting.SAVE_PATH + time + ex, 'wb') as fo:
+                fo.write(content)       
+        except PermissionError as ex:
+            return f'サーバ側でエラーが発生しました。\n開発者に連絡してください\n{ex}'
+        return 'uploadが完了しました!'
     else:
         return 'GET'
 
 
 if __name__ == '__main__':
-    # app.run(debug=False, host='0.0.0.0', port=80)
-    app.run(host="0.0.0.0", port=80)
+    # flaskのデフォルトポートは5000番
+    app.run(host=server_setting.SAVE_SERVER_IP)
+    # 外部とつなげる場合はポートを80に設定する
+    # app.run(host=server_setting.SAVE_SERVER_IP, port=80)
